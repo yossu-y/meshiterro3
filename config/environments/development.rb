bfe01cdb158137014a66a82ec8d00715724cf954
yossu-y <yangyuub@gmail.com>
2022-05-06 09:12:05 +0000

    [start]meshiterro3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "4f5fad0c7d7c4a9a8aab6ac762d5c2fa.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
