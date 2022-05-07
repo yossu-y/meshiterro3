8fb8cac32aaaf637344f791970699965f2913329
yossu-y <yangyuub@gmail.com>
2022-05-07 05:44:39 +0000

    [finish]9章

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/top" => "homes#top",as: "about"
+  
+  resources :post_images,only:[:index, :new, :show]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
