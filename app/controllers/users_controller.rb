187f282a67461dc49a7775bf5d54126b43e79e0b
yossu-y <yangyuub@gmail.com>
2022-05-07 11:37:40 +0000

    [finish]16章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to edit_user_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
 end
